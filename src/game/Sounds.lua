718f49c0088a7cba70a4221a96c1ec94b416593f
cnajayi <112961125+cnajayi@users.noreply.github.com>
2025-05-21 02:34:55 -0400

    Update Sounds.lua

--- src/game/Sounds.lua
+++ src/game/Sounds.lua
@@ -5,6 +5,8 @@ sounds["music_adventure"] = love.audio.newSource("sounds/timbeek/8Bit_Adventure.
 sounds["music_adventure"]:setVolume(0.3)
 sounds["music_surfrock"] = love.audio.newSource("sounds/timbeek/Surf_Rock_Light.wav","static")
 sounds["music_surfrock"]:setVolume(0.3)
+sounds["exploration"] = love.audio.newSource("sounds/exploration.mp3","static")
+sounds["exploration"]:setVolume(0.3)
 -- Add this to the file
 sounds["attack1"] = love.audio.newSource("sounds/leohpaz/Slash.wav","static")
 sounds["attack2"] = love.audio.newSource("sounds/leohpaz/Claw.wav","static")
