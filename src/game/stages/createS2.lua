088aed282f98856dc6492018a2616270e2ea03b5
cnajayi <112961125+cnajayi@users.noreply.github.com>
2025-05-21 02:34:26 -0400

    Update createS2.lua

--- src/game/stages/createS2.lua
+++ src/game/stages/createS2.lua
@@ -5,34 +5,39 @@ local Boar = require "src.game.mobs.Boar"
 local Sounds = require "src.game.Sounds"
 
 local function createS2()
-    local stage = Stage(20, 50, BasicTileset)
-    local mapdata = require "src.game.maps.map2"
+    local stage = Stage(20,50,BasicTileset)
+    local mapdata = require "src.game.maps.map1"
     stage:readMapData(mapdata)
 
     local objdata = require "src.game.maps.map2objects"
     stage:readObjectsData(objdata)
 
-    --backgrounds
+    -- Backgrounds
     local bg1 = Background("graphics/tilesets/FreeCute/BG1.png")
     local bg2 = Background("graphics/tilesets/FreeCute/BG2.png")
     local bg3 = Background("graphics/tilesets/FreeCute/BG3.png")
-
+    
     stage:addBackground(bg1)
     stage:addBackground(bg2)
     stage:addBackground(bg3)
 
-    --initial player pos
+    -- Initial Player Pos
     stage.initialPlayerX = 1*16
     stage.initialPlayerY = 13*16 
 
-    --mobs
+    -- Adding mobs
     local mob1 = Boar()
-    mob1:setCoord(25*16, 12*16)
+    mob1:setCoord(25 * 16, 10 * 16)
     mob1:changeDirection()
     stage:addMob(mob1)
 
-    --background music
-    stage:setMusic(Sounds["music_adventure"])
+    local mob2 = Boar()
+    mob1:setCoord(40*16, 12*16)
+    mob1:changeDirection()
+    stage:addMob(mob1)
+
+    -- music
+    stage:setMusic(Sounds["exploration"])
 
     return stage
 end
